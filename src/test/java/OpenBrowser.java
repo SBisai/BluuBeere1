70213cbb19f038c2b008e77972234b2aac6b978c
Soumitra_Bisai <soumitrabisai.jgec@gamil.com>
2019-06-06 17:39:25 +0530

    Bouild Jun0619

---
+++ src/test/java/OpenBrowser.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OpenBrowser {
+	
+	@Test
+	 public static void openBrowser() {
+		
+		System.setProperty("webdriver.chrome.driver", "G:\\Java\\Bluubeere\\drivers\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.google.com/");
+		driver.close();
+	}
+
+}
