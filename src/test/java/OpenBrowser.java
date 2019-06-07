6d9a3c7b0ff4a4be649077876bddcdf3b13bd374
Soumitra_Bisai <soumitrabisai.jgec@gamil.com>
2019-06-07 23:37:03 +0530

    open browser updated

--- src/test/java/OpenBrowser.java
+++ src/test/java/OpenBrowser.java
@@ -13,7 +13,7 @@ public class OpenBrowser {
 		driver.manage().window().maximize();
 		
 		driver.get("https://www.google.com/");
-		System.out.println("**************Done*****************")
+		System.out.println("**************Done*****************");
 		driver.close();
 	}
 
